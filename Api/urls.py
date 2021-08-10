1998aa127304ee56c63891d9b0e613a88977b83b
gideon <koechgideon70@gmail.com>
2021-08-10 03:17:45 +0300

    simple django app with CRUD operations

--- Api/urls.py
+++ Api/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns=[
-    path('', views.ApiOverview, name='api-overview'), 
+    path('', views.ApiOverview, name='api-overview'),
+    path('tasklist/', views.TaskList, name='task-list'),
+    path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+	path('task-create/', views.taskCreate, name="task-create"),
+
+	path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
+	path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),  
 ]
--- Api/views.py
+++ Api/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-#from .serializers import TaskSerializer
+from rest_framework.utils import serializer_helpers
+from .serializers import TaskSerializer
+from .models import Task
 
 @api_view(['GET'])
 def ApiOverview(request):
@@ -16,3 +18,43 @@ def ApiOverview(request):
     }
     
     return Response(api_urls)
+
+@api_view(['GET'])
+def TaskList(request):
+    tasks=Task.objects.all()
+    serializer=TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+	tasks = Task.objects.get(id=pk)
+	serializer = TaskSerializer(tasks, many=False)
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+	serializer = TaskSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+	task = Task.objects.get(id=pk)
+	serializer = TaskSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+	task = Task.objects.get(id=pk)
+	task.delete()
+
+	return Response('Item succsesfully delete!')
